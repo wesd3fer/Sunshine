cddacbdbc4aaf36718fee193f9db5638d6cb233e
Nicholas <nfragiskatos@gmail.com>
2015-02-08 19:09:41 -0700

    asdf

--- app/src/main/java/com/example/nicholas/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/nicholas/sunshine/app/MainActivity.java
@@ -1,21 +1,9 @@
 package com.example.nicholas.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -26,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -54,53 +42,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] forecastArray = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Cloudy - 75/60",
-                    "Saturday - Rainy - 72/65",
-                    "Sunday - Rainy - 72/65",
-                    "Monday - Rainy - 72/65",
-                    "Tuesday - Rainy - 72/65",
-                    "Wednesday - Rainy - 72/65",
-                    "Thursday - Rainy - 72/65",
-                    "Friday - Rainy - 72/65",
-                    "Saturday - Rainy - 72/65",
-                    "Sunday - Rainy - 72/65",
-                    "Monday - Rainy - 72/65",
-                    "Tuesday - Rainy - 72/65",
-                    "Wednesday - Rainy - 72/65",
-                    "Thursday - Rainy - 72/65",
-                    "Friday - Rainy - 72/65",
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-            ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
-                    // Current layout context (This Activity)
-                    getActivity(),
-                    // The name of the layout id that contains the view we will populate
-                    R.layout.list_item_forecast,
-                    // The id of the textview to populate in the list item layout
-                    R.id.list_item_forecast_textview,
-                    // Data to populate the list
-                    weekForecast);
-
-            // Get a reference to the ListView and attach the adapter to it for displaying
-            ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
-            listView.setAdapter(forecastAdapter);
-            return rootView;
-        }
-    }
 }
